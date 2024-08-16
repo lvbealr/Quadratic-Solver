bb7cd896f5ad838dcb9d28bd7b2678a3f9dda6b7
lvbealr <lvbealr@mail.ru>
2024-08-16 16:19:56 +0300

    Braces

--- SquareSolver/main.c
+++ SquareSolver/main.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
 #include <math.h>
 
-void PrintRoots(int code, double roots[2])
-{
-    switch (code)
-    {
+void PrintRoots(int code, double roots[2]) {
+    switch (code) {
         case 0: printf("Infinitely many solutions"); break;
         case 1: printf("No solutions"); break;
         case 2: printf("It is linear (non-quadratic) equation\n[0] Solution: x = %lf", roots[0]); break;
@@ -13,50 +11,39 @@ void PrintRoots(int code, double roots[2])
         case 5: printf("D > 0\n[0] Solution: x = %lf\n[1] Solution: x = %lf", roots[0], roots[1]); break;
     }
 }
-int SquareSolver(double const a, double const b, double const c, double roots[2])
-{
+int SquareSolver(double const a, double const b, double const c, double roots[2]) {
     const double eps = 1e-07;
-    if (fabs(a) <= eps)
-    {
-        if (fabs(b) <= eps)
-        {
-            if (fabs(c) <= eps)
-            {
+    if (fabs(a) <= eps) {
+        if (fabs(b) <= eps) {
+            if (fabs(c) <= eps) {
                 return 0;
             }
-            else
-            {
+            else {
                 return 1;
             }
         }
-        else
-        {
+        else {
             roots[0] = -c/b;
             return 2;
         }
     }
-    else
-    {
+    else {
         double const discriminant = b*b - 4*a*c;
-        if (discriminant < -eps)
-        {
+        if (discriminant < -eps) {
             return 3;
         }
-        else if (fabs(discriminant) <= eps)
-        {
+        else if (fabs(discriminant) <= eps) {
             roots[0] = -b/2/a;
             return 4;
         }
-        else
-        {
+        else {
             roots[0] = (-b + sqrt(discriminant))/2/a; roots[1] = (-b -sqrt(discriminant))/2/a;
             return 5;
         }
     }
 }
 
-int main(void)
-{
+int main(void) {
     double a = NAN, b = NAN, c = NAN;
     double roots[2] = {NAN, NAN};
     printf("Input values of a, b, c coefficients:");
