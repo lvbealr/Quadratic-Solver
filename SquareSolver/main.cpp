6310136641f61275cbabca17c574c8c5e9555e20
lvbealr <lvbealr@mail.ru>
2024-08-17 21:03:07 +0300

    pushRoot & errorPushCode

--- SquareSolver/main.cpp
+++ SquareSolver/main.cpp
@@ -3,36 +3,6 @@
 
 const double EPS = 1e-07;
 
-/*/ Start of rootList Struct /*/
-const int MAX_COUNT = 2;
-
-struct rootList {
-    int count = 0;
-    double roots[MAX_COUNT]{};
-};
-
-void rootListInitialize(rootList *rL) {
-    for (int i = 0; i < MAX_COUNT; i++) {
-        rL->roots[i] = NAN;
-    }
-}
-void push(rootList *rL, double pushRoot) {
-    if (rL->count < MAX_COUNT) {
-        rL->roots[rL->count] = pushRoot;
-        rL->count++;
-    }
-    else {
-        printf("Error: rootList is full");
-    }
-}
-
-void get(rootList *rL) {
-    for (int i = 0; i < rL->count; i++) {
-        printf("%lf\t", rL->roots[i]);
-    }
-}
-/*/ End of rootList struct /*/
-
 enum solveCode {
     LINEAR_INF_ROOTS = 0, // a, b, c = 0
     LINEAR_NO_ROOTS = 1, // a, b = 0; c != 0
@@ -42,8 +12,64 @@ enum solveCode {
     QUADRATIC_TWO_ROOTS = 5 // D > 0
 };
 
-void printRoots(solveCode code, rootList rL) {
-    switch (code) {
+/*/ Start of rootList Struct /*/
+const int MAX_COUNT = 2;
+
+struct rootList {
+    int count = 0;
+    double roots[MAX_COUNT] = {};
+    solveCode status;
+};
+
+void rootListInitialize(rootList *rL) {
+    for (int i = 0; i < MAX_COUNT; i++) {
+        rL->roots[i] = NAN;
+    }
+}
+
+enum errorPushCode {
+    /*/ CODE of SUCCESS, CODE of FAILURE/*/
+};
+
+int pushRoot(rootList *rL, double Root) {
+    switch (rL->status) {
+        case LINEAR_INF_ROOTS: {} break;
+        case LINEAR_NO_ROOTS: {} break;
+        case LINEAR_ONE_ROOT: {
+            if (rL->count < 1) {
+                rL->roots[rL->count] = Root;
+                rL->count++;
+                return {}; // OK
+            }
+        } break;
+        case QUADRATIC_NO_ROOTS: {} break;
+        case QUADRATIC_ONE_ROOT: {
+            if (rL->count < 1) {
+                rL->roots[rL->count] = Root;
+                rL->count++;
+                return {}; // OK
+            }
+        } break;
+        case QUADRATIC_TWO_ROOTS: {
+            if (rL->count < 2) {
+                rL->roots[rL->count] = Root;
+                rL->count++;
+                return {}; // OK
+            }
+        }
+    }
+    return {}; // ERROR
+}
+
+void printRoot(rootList *rL) {
+    for (int i = 0; i < rL->count; i++) {
+        printf("%lf\t", rL->roots[i]);
+    }
+}
+/*/ End of rootList struct /*/
+
+void printResult(rootList rL) {
+    switch (rL.status) {
         case LINEAR_INF_ROOTS: printf("Infinitely many solutions"); break;
         case LINEAR_NO_ROOTS: printf("No solutions"); break;
         case LINEAR_ONE_ROOT: printf("It is linear (non-quadratic) equation\nSolutions: "); break;
@@ -51,52 +77,47 @@ void printRoots(solveCode code, rootList rL) {
         case QUADRATIC_ONE_ROOT: printf("D = 0\nSolutions: "); break;
         case QUADRATIC_TWO_ROOTS: printf("D > 0\nSolutions: "); break;
     }
-    get(&rL);
+    printRoot(&rL);
 }
 
-solveCode Code;
-
-solveCode linearSolver(double const b, double const c, rootList &rL) {
+void linearSolver(double const b, double const c, rootList &rL) {
     if (fabs(b) <= EPS) {
         if (fabs(c) <= EPS) {
-            Code = LINEAR_INF_ROOTS;
+            rL.status = LINEAR_INF_ROOTS;
         }
         else {
-            Code = LINEAR_NO_ROOTS;
+            rL.status = LINEAR_NO_ROOTS;
         }
     }
     else {
-        push(&rL, -c/b);
-        Code = LINEAR_ONE_ROOT;
+        rL.status = LINEAR_ONE_ROOT;
+        pushRoot(&rL, -c/b);
     }
-    return Code;
 }
 
-solveCode squareSolver(double const a, double const b, double const c, rootList &rL) {
+void squareSolver(double const a, double const b, double const c, rootList &rL) {
     double const discriminant = b*b - 4*a*c;
     if (discriminant < -EPS) {
-        Code = QUADRATIC_NO_ROOTS;
+        rL.status = QUADRATIC_NO_ROOTS;
     }
     else if (fabs(discriminant) <= EPS) {
-        push(&rL, -b/2/a);
-        Code = QUADRATIC_ONE_ROOT;
+        rL.status = QUADRATIC_ONE_ROOT;
+        pushRoot(&rL, -b/2/a);
     }
     else {
-        push(&rL, (-b + sqrt(discriminant))/2/a);
-        push(&rL, (-b -sqrt(discriminant))/2/a);
-        Code = QUADRATIC_TWO_ROOTS;
+        rL.status = QUADRATIC_TWO_ROOTS;
+        pushRoot(&rL, (-b + sqrt(discriminant))/2/a);
+        pushRoot(&rL, (-b -sqrt(discriminant))/2/a);
     }
-    return Code;
 }
 
-solveCode Solve(double const a, double const b, double const c, rootList &rL) {
+void Solve(double const a, double const b, double const c, rootList &rL) {
     if (fabs(a) <= EPS) {
-        Code = linearSolver(b, c, rL);
+        linearSolver(b, c, rL);
     }
     else {
-        Code = squareSolver(a, b, c, rL);
+        squareSolver(a, b, c, rL);
     }
-    return Code;
 }
 
 int main() {
@@ -105,7 +126,7 @@ int main() {
     double a = NAN, b = NAN, c = NAN;
     printf("Input values of a, b, c coefficients:");
     scanf("%lf %lf %lf", &a, &b, &c);
-    solveCode code = Solve(a, b, c, rL);
-    printRoots(code, rL);
+    Solve(a, b, c, rL);
+    printResult(rL);
     return 0;
 }
