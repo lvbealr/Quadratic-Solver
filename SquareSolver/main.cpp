2035d131e11eaf86e796f7f3861c6a69f2e26e8f
lvbealr <lvbealr@mail.ru>
2024-08-17 22:12:12 +0300

    Update main.cpp

--- SquareSolver/main.cpp
+++ SquareSolver/main.cpp
@@ -27,14 +27,11 @@ void rootListInitialize(rootList *rL) {
     }
 }
 
-enum errorPushCode {
-    /*/ CODE of SUCCESS, CODE of FAILURE/*/
-};
-
-int pushRoot(rootList *rL, double Root) {
+bool pushRoot(rootList *rL, double Root) {
     switch (rL->status) {
-        case LINEAR_INF_ROOTS: {} break;
-        case LINEAR_NO_ROOTS: {} break;
+        case LINEAR_INF_ROOTS:
+        case LINEAR_NO_ROOTS:
+        case QUADRATIC_NO_ROOTS: {} break;
         case LINEAR_ONE_ROOT: {
             if (rL->count < 1) {
                 rL->roots[rL->count] = Root;
@@ -42,7 +39,6 @@ int pushRoot(rootList *rL, double Root) {
                 return {}; // OK
             }
         } break;
-        case QUADRATIC_NO_ROOTS: {} break;
         case QUADRATIC_ONE_ROOT: {
             if (rL->count < 1) {
                 rL->roots[rL->count] = Root;
@@ -50,15 +46,14 @@ int pushRoot(rootList *rL, double Root) {
                 return {}; // OK
             }
         } break;
-        case QUADRATIC_TWO_ROOTS: {
+        case QUADRATIC_TWO_ROOTS:
             if (rL->count < 2) {
                 rL->roots[rL->count] = Root;
                 rL->count++;
                 return {}; // OK
             }
         }
-    }
-    return {}; // ERROR
+    return false;
 }
 
 void printRoot(rootList *rL) {
@@ -68,50 +63,50 @@ void printRoot(rootList *rL) {
 }
 /*/ End of rootList struct /*/
 
-void printResult(rootList rL) {
-    switch (rL.status) {
+void printResult(rootList *rL) {
+    switch (rL->status) {
         case LINEAR_INF_ROOTS: printf("Infinitely many solutions"); break;
         case LINEAR_NO_ROOTS: printf("No solutions"); break;
         case LINEAR_ONE_ROOT: printf("It is linear (non-quadratic) equation\nSolutions: "); break;
-        case QUADRATIC_NO_ROOTS: printf("D < 0: no real solutions"); break;
+        case QUADRATIC_NO_ROOTS: printf("D < 0: No real solutions"); break;
         case QUADRATIC_ONE_ROOT: printf("D = 0\nSolutions: "); break;
         case QUADRATIC_TWO_ROOTS: printf("D > 0\nSolutions: "); break;
     }
-    printRoot(&rL);
+    printRoot(rL);
 }
 
-void linearSolver(double const b, double const c, rootList &rL) {
+void linearSolver(double const b, double const c, rootList *rL) {
     if (fabs(b) <= EPS) {
         if (fabs(c) <= EPS) {
-            rL.status = LINEAR_INF_ROOTS;
+            rL->status = LINEAR_INF_ROOTS;
         }
         else {
-            rL.status = LINEAR_NO_ROOTS;
+            rL->status = LINEAR_NO_ROOTS;
         }
     }
     else {
-        rL.status = LINEAR_ONE_ROOT;
-        pushRoot(&rL, -c/b);
+        rL->status = LINEAR_ONE_ROOT;
+        pushRoot(rL, -c/b);
     }
 }
 
-void squareSolver(double const a, double const b, double const c, rootList &rL) {
+void squareSolver(double const a, double const b, double const c, rootList *rL) {
     double const discriminant = b*b - 4*a*c;
     if (discriminant < -EPS) {
-        rL.status = QUADRATIC_NO_ROOTS;
+        rL->status = QUADRATIC_NO_ROOTS;
     }
     else if (fabs(discriminant) <= EPS) {
-        rL.status = QUADRATIC_ONE_ROOT;
-        pushRoot(&rL, -b/2/a);
+        rL->status = QUADRATIC_ONE_ROOT;
+        pushRoot(rL, -b/2/a);
     }
     else {
-        rL.status = QUADRATIC_TWO_ROOTS;
-        pushRoot(&rL, (-b + sqrt(discriminant))/2/a);
-        pushRoot(&rL, (-b -sqrt(discriminant))/2/a);
+        rL->status = QUADRATIC_TWO_ROOTS;
+        pushRoot(rL, (-b + sqrt(discriminant))/2/a);
+        pushRoot(rL, (-b -sqrt(discriminant))/2/a);
     }
 }
 
-void Solve(double const a, double const b, double const c, rootList &rL) {
+void Solve(double const a, double const b, double const c, rootList *rL) {
     if (fabs(a) <= EPS) {
         linearSolver(b, c, rL);
     }
@@ -126,7 +121,7 @@ int main() {
     double a = NAN, b = NAN, c = NAN;
     printf("Input values of a, b, c coefficients:");
     scanf("%lf %lf %lf", &a, &b, &c);
-    Solve(a, b, c, rL);
-    printResult(rL);
+    Solve(a, b, c, &rL);
+    printResult(&rL);
     return 0;
 }
